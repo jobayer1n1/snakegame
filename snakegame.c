fffb6d14c413dcf91569e87cf6b50f457141738a
jobayerahmedmishu <141646914+jobayerahmedmishu@users.noreply.github.com>
2024-05-27 02:04:49 +0600

    Update snakegame.c

--- snakegame.c
+++ snakegame.c
@@ -1,10 +1,12 @@
-// C program to build the complete
-// snake game
 #include <conio.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <time.h>
 
+void set_up();
+void draw();
+void logic();
 void main_interface();
 void print_highscore();
 void play();
@@ -18,124 +20,7 @@ int i, j, height = 20, width = 20,speed=0.01;
 int gameover, score;
 int x, y, fruitx, fruity, flag;
 
-// Function to generate the fruit
-// within the boundary
-void setup()
-{
-	gameover = 0;
 
-	// Stores height and width
-	x = height / 2;
-	y = width / 2;
-label1:
-	fruitx = rand() % 20;
-	if (fruitx == 0)
-		goto label1;
-label2:
-	fruity = rand() % 20;
-	if (fruity == 0)
-		goto label2;
-	score = 0;
-}
-
-// Function to draw the boundaries
-void draw()
-{
-	system("cls");
-	for (i = 0; i < height; i++) {
-		for (j = 0; j < width; j++) {
-			if (i == 0 || i == width - 1
-				|| j == 0
-				|| j == height - 1) {
-				printf("#");
-			}
-			else {
-				if (i == x && j == y)
-					printf("0");
-				else if (i == fruitx
-						&& j == fruity)
-					printf("*");
-				else
-					printf(" ");
-			}
-		}
-		printf("\n");
-	}
-
-	printf("Score : %d",score);
-
-}
-
-// Function to take the input
-void input()
-{
-	if (kbhit()) {
-		switch (getch()) {
-		case 'a':
-			flag = 1;
-			break;
-		case 's':
-			flag = 2;
-			break;
-		case 'd':
-			flag = 3;
-			break;
-		case 'w':
-			flag = 4;
-			break;
-		case 'x':
-			gameover = 1;
-			break;
-		}
-	}
-}
-
-// Function for the logic behind
-// each movement
-void logic()
-{
-	sleep(speed);
-	switch (flag) {
-	case 1:
-		y--;
-		break;
-	case 2:
-		x++;
-		break;
-	case 3:
-		y++;
-		break;
-	case 4:
-		x--;
-		break;
-	default:
-		break;
-	}
-
-	// If the game is over
-	if (x <= 0 || x >= height
-		|| y <= 0 || y >= width)
-		gameover = 1;
-
-	// If snake reaches the fruit
-	// then update the score
-	if (x == fruitx && y == fruity) {
-	label3:
-		fruitx = rand() % 20;
-		if (fruitx == 0)
-			goto label3;
-
-	// After eating the above fruit
-	// generate new fruit
-	label4:
-		fruity = rand() % 20;
-		if (fruity == 0)
-			goto label4;
-		score += 10;
-	}
-}
-
-// Driver Code
 void main()
 {
     highscore_initialize();
@@ -181,6 +66,116 @@ void main()
 
 }
 
+
+
+void setup()
+{
+    srand(time(NULL));
+	gameover = 0;
+
+	x = height / 2;
+	y = width / 2;
+
+label1:
+	fruitx = rand() % 20;
+	if (fruitx == 0)
+		goto label1;
+label2:
+	fruity = rand() % 20;
+	if (fruity == 0)
+		goto label2;
+	score = 0;
+}
+
+void draw()
+{
+	system("cls");
+	for (i = 0; i < height; i++) {
+		for (j = 0; j < width; j++) {
+			if (i == 0 || i == width - 1
+				|| j == 0
+				|| j == height - 1) {
+				printf("#");
+			}
+			else {
+				if (i == x && j == y)
+					printf("0");
+				else if (i == fruitx
+						&& j == fruity)
+					printf("*");
+				else
+					printf(" ");
+			}
+		}
+		printf("\n");
+	}
+
+	printf("Score : %d",score);
+
+}
+
+void input()
+{
+	if (kbhit()) {
+		switch (getch()) {
+		case 'a':
+			flag = 1;
+			break;
+		case 's':
+			flag = 2;
+			break;
+		case 'd':
+			flag = 3;
+			break;
+		case 'w':
+			flag = 4;
+			break;
+		case 'x':
+			gameover = 1;
+			break;
+		}
+	}
+}
+
+
+void logic()
+{
+	sleep(speed);
+	switch (flag) {
+	case 1:
+		y--;
+		break;
+	case 2:
+		x++;
+		break;
+	case 3:
+		y++;
+		break;
+	case 4:
+		x--;
+		break;
+	default:
+		break;
+	}
+
+	if (x <= 0 || x >= height
+		|| y <= 0 || y >= width)
+		gameover = 1;
+
+	if (x == fruitx && y == fruity) {
+	label3:
+		fruitx = rand() % 20;
+		if (fruitx == 0)
+			goto label3;
+
+	label4:
+		fruity = rand() % 20;
+		if (fruity == 0)
+			goto label4;
+		score += 10;
+	}
+}
+
 void main_interface()
 {
     printf("1 --> play\n");
@@ -196,10 +191,8 @@ void play()
 
     setup();
 
-	// Until the game is over
-	while (!gameover) {
-
-		// Function Call
+	while (!gameover)
+    {
 		draw();
 		input();
 		logic();
